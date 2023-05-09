f562059d9dc9babab4639760decf57c82c28f925
Max <KontinentKM@gmail.com>
2023-05-09 10:57:00 +0300

    refactor: refactor 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+const filesСopyFolder = path.join(__dirname, 'files-copy');
+
 
 const copyDir = async () => {
-  await fs.promises.mkdir('04-copy-directory/files-copy', { recursive: true });
+  await fs.promises.rm(filesСopyFolder, { recursive: true, force: true });
+  await fs.promises.mkdir(filesСopyFolder, { recursive: true });
 
   const files = await fs.promises.readdir('04-copy-directory/files');
 
   for (const file of files) {
     const source = path.join('04-copy-directory/files', file);
-    const destination = path.join('04-copy-directory/files-copy', file);
+    const destination = path.join(filesСopyFolder, file);
     await fs.promises.copyFile(source, destination);
   }
 };
